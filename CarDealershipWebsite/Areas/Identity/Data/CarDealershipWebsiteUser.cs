f9ce8ed6284b20fe425208e1ea317f27cf94eed7
Muhammad Amar <ac116681@avcol.school.nz>
2023-08-07 17:06:41 +1200

    missed commit

--- CarDealershipWebsite/Areas/Identity/Data/CarDealershipWebsiteUser.cs
+++ CarDealershipWebsite/Areas/Identity/Data/CarDealershipWebsiteUser.cs
@@ -12,7 +12,7 @@ public class CarDealershipWebsiteUser : IdentityUser
 {
     [Required] // requires user to fill in 
     public string FirstName { get; set; }
-    [Required] // requires user to fill in 
+    [Required] // requires user to fill in
     public string LastName { get; set; }
 }
 
